7bd9398f1dca1eb15c3867ed90c6834c89a5fe3f
amanb3011 <aman.bhandari.3011@gmail.com>
2023-10-19 00:10:56 +0530

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Container } from 'react-bootstrap' ;
+import Header from './components/header';
+import Footer from './components/footer';
 
 const App = () => {
   return (
-    <h1>
-      welcome to eshop
-    </h1>
-  )
-}
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+      <h1>
+      welcome to eshop 
+      </h1>
+      </Container>
+    </main> 
+    <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
