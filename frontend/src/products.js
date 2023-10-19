1b4419cb44dddb7c6c11ea2302c4b8c7be21e226
amanb3011 <aman.bhandari.3011@gmail.com>
2023-10-19 21:32:58 +0530

    added homescreen

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -1,5 +1,6 @@
 const products = [
   {
+    _id: '1',
     name: 'Airpods Wireless Bluetooth Headphones',
     image: '/images/airpods.jpg',
     description:
@@ -12,10 +13,11 @@ const products = [
     numReviews: 12,
   },
   {
-    name: 'iPhone 13 Pro 256GB Memory',
+    _id: '2',
+    name: 'iPhone 11 Pro 256GB Memory',
     image: '/images/phone.jpg',
     description:
-      'Introducing the iPhone 13 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     brand: 'Apple',
     category: 'Electronics',
     price: 599.99,
@@ -24,6 +26,7 @@ const products = [
     numReviews: 8,
   },
   {
+    _id: '3',
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.jpg',
     description:
@@ -36,7 +39,8 @@ const products = [
     numReviews: 12,
   },
   {
-    name: 'Sony Playstation 5',
+    _id: '4',
+    name: 'Sony Playstation 4 Pro White Version',
     image: '/images/playstation.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
@@ -48,6 +52,7 @@ const products = [
     numReviews: 12,
   },
   {
+    _id: '5',
     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
@@ -60,6 +65,7 @@ const products = [
     numReviews: 10,
   },
   {
+    _id: '6',
     name: 'Amazon Echo Dot 3rd Generation',
     image: '/images/alexa.jpg',
     description:
@@ -71,6 +77,6 @@ const products = [
     rating: 4,
     numReviews: 12,
   },
-];
+]
 
-export default products;
+export default products
