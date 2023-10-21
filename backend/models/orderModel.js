a25c60ebea8b139c2dda0f732e23739cb45d9f5c
amanb3011 <aman.bhandari.3011@gmail.com>
2023-10-21 12:12:53 +0530

    added pproducts from database

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const Order = new mongoose.Schema({
+const orderSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
