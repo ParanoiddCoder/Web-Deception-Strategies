2f9834fd73cf9f2d6886d4577f5fb582469540d7
amanb3011 <aman.bhandari.3011@gmail.com>
2023-11-03 20:26:49 +0530

    backend authentication using jwt

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,176 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js';
+
+// @desc    auth user & get token 
+// @route POST /api/users/login
+// @access public
+const authUser = asyncHandler(async(req,res) => {
+    const {email,password} = req.body;
+
+    const user = await User.findOne({email});
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res,user._id);
+       
+        res.status(200).json({
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            isAdmin : user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password')
+    }
+});
+// const authUser = asyncHandler(async (req, res) => {
+//     const { email, password } = req.body;
+  
+//     const user = await User.findOne({ email });
+  
+//     if (user && (await user.matchPassword(password))) {
+//       generateToken(res, user._id);
+  
+//       res.json({
+//         _id: user._id,
+//         name: user.name,
+//         email: user.email,
+//         isAdmin: user.isAdmin,
+//       });
+//     } else {
+//       res.status(401);
+//       throw new Error('Invalid email or password');
+//     }
+//   });
+  
+// @desc    register user
+// @route POST /api/users
+// @access public
+const registerUser = asyncHandler(async(req,res) => {
+    const {name,email,password} = req.body;
+
+    const userExists = User.findOne({email});
+
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    if(user)
+    {
+        generateToken(res,user._id);
+        res.status(201).json({
+            _id : user._id,
+            name : user.name,
+            email : user.email,
+            isAdmin : user.isAdmin,
+        });
+    }
+});
+
+// @desc    user / clear cookie
+// @route POST /api/users/logout
+// @access private
+const logoutUser = asyncHandler(async(req,res) => {
+    res.cookie('jwt', '' , {
+        httpOnly : true,
+        expires: new Date(0),// 30 days
+    });
+
+    res.status(200).json({ message: 'Logged out successfully'});
+});
+
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async(req,res) => {
+    const user = await User.findById(req.user._id);
+
+    if(user){
+    res.status(200).json({
+        _id : user._id,
+        name : user.name,
+        email : user.email,
+        isAdmin : user.isAdmin,
+    });
+    } else {
+        res.status(404);
+        throw new Error('user not found'); 
+    }
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async(req,res) => {
+    const user = User.findById(req.user._id);
+
+    if(user){
+        user.name = req.body.name||user.name;
+        user.email = req.body.email||user.email;
+    if(req.body.password){
+        user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+        _id : updateUser._id,
+        name : updateUser.name,
+        email : updateUser.email,
+        isAdmin : updateUser.isAdmin,
+    });
+    }
+    else {
+        res.status(404);
+        throw new Error('user not found'); 
+    }
+});
+
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async(req,res) => {
+    res.json('get users');
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async(req,res) => {
+    res.json('get user by id');
+});
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async(req,res) => {
+    res.json('delete user');
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async(req,res) => {
+    res.json('update user');
+});
+
+export {
+    authUser,
+    registerUser,
+    updateUser,
+    updateUserProfile,
+    deleteUser,
+    getUserProfile,
+    getUsers,
+    getUserById,
+    logoutUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
-import pRoutes from './routes/productRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -11,11 +13,17 @@ connectDB();
 
 const app = express();//initialising express
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser()); 
+
 app.get('/',(req,res) => {
     res.send('API is running...');
 });
 
-app.use('/api/products',pRoutes);
+app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
